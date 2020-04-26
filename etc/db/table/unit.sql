ad542cccaa8324ff19aacd2591a7dc91ad1e9848
misterhaan <misterhaan@track7.org>
2020-04-26 12:18:27 -0500

    make unit abbr optional

--- etc/db/table/unit.sql
+++ etc/db/table/unit.sql
@@ -1,7 +1,7 @@
 create table if not exists unit (
 	id tinyint unsigned not null primary key auto_increment,
 	measure tinyint unsigned not null default 0 comment 'Units can only be converted among the same measure.  Zero for no conversion, 1-volume, 2-weight',
-	abbr varchar(8) not null comment 'Displayed for ingredient amounts',
+	abbr varchar(8) not null default '' comment 'Displayed for ingredient amounts.  If bkank, name is used.',
 	unique(measure, abbr),
 	name varchar(24) not null comment 'Longer name used as tooltip for abbreviation',
 	unique(measure, name),
