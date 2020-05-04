fbcdc48500d29bb42ff061294509ce629cf09d63
misterhaan <misterhaan@track7.org>
2020-05-04 17:38:22 -0500

    go home when clicking icon or site title

--- module/component/titlebar.js
+++ module/component/titlebar.js
@@ -6,6 +6,11 @@ export default {
 	props: [
 		"hideSearch"
 	],
+	methods: {
+		GoHome() {
+			location.hash = "";
+		}
+	},
 	mixins: [
 		ReportError
 	],
@@ -14,8 +19,8 @@ export default {
 	},
 	template: /*html*/ `
 		<header id=titlebar>
-			<img id=favicon src=favicon.svg alt="">
-			<h2 id=sitetitle>${AppName.Full}</h2>
+			<img id=favicon src=favicon.svg alt="" @click=GoHome>
+			<h2 id=sitetitle @click=GoHome>${AppName.Full}</h2>
 			<recipeSearch v-if=!hideSearch @error=Error($event)></recipeSearch>
 		</header>
 `
